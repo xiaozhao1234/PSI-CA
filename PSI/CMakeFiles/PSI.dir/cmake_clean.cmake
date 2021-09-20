6e27c454ead779c7baafcba934356ae0a4d167e0
xiaozhao1234 <49443947+xiaozhao1234@users.noreply.github.com>
2021-09-20 22:45:56 +0800

    Add files via upload

---
+++ PSI/CMakeFiles/PSI.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PSI.dir/src/PsiReceiver.cpp.o"
+  "CMakeFiles/PSI.dir/src/PsiSender.cpp.o"
+  "CMakeFiles/PSI.dir/src/utils.cpp.o"
+  "../lib/libPSI.pdb"
+  "../lib/libPSI.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PSI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
